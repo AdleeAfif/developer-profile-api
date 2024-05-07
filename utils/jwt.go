0ffcc572f381a9bad328038684f8ec5321acf090
AdleeAfif <kamitokazuya@gmail.com>
2024-05-07 11:26:42 +0800

    Added hasing and jwt for auth purpose

---
+++ utils/jwt.go
@@ -0,0 +1,52 @@
+package utils
+
+import (
+	"errors"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+const secretKey = "secret"
+
+func GenerateToken(email string, role string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email": email,
+		"role":  role,
+		"exp":   time.Now().Add(time.Hour * 2).Unix(),
+	})
+
+	return token.SignedString([]byte(secretKey))
+}
+
+func ValidateToken(token string) (string, error) {
+	token = strings.Split(token, " ")[1]
+	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+
+		return []byte(secretKey), nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	if !parsedToken.Valid {
+		return "", errors.New("invalid token")
+	}
+
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+
+	if !ok {
+		return "", errors.New("invalid token claims")
+	}
+
+	email := claims["email"].(string)
+
+	return email, nil
+}
